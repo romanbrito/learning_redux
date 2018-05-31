5c0773c6542ca6614234043618ff4abba2444e43
Roman Brito <romanbrito1@gmail.com>
2018-05-31 18:22:29 -0500

    connecting more components

--- src/components/containers/GoalProgress.js
+++ src/components/containers/GoalProgress.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import GoalProgress from '../ui/GoalProgress'
+import {connect} from 'react-redux'
+import {setGoal} from "../../actions";
 
-export default () =>
-    <GoalProgress current={10} 
-                  goal={20}
-                  onNewGoal={goal => console.log('todo: change goal', goal)} />
+const mapStateToProps = state =>
+  ({
+    current: state.allSkiDays.length,
+    goal: state.goal
+  })
+
+const mapDispatchToProps = dispatch =>
+  ({
+    onNewGoal(goal) {
+      dispatch(
+        setGoal(goal)
+      )
+    }
+
+  })
+
+export default connect(mapStateToProps, mapDispatchToProps)(GoalProgress)
+// export default () =>
+//     <GoalProgress current={10}
+//                   goal={20}
+//                   onNewGoal={goal => console.log('todo: change goal', goal)} />
