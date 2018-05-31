bc5810d458ed819e2636c6e4b7a58914efddcdd1
Roman Brito <romanbrito1@gmail.com>
2018-05-31 15:35:38 -0500

    map props to React components with react-redux {connect}

--- src/index.js
+++ src/index.js
@@ -3,10 +3,28 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import {BrowserRouter} from 'react-router-dom'
 import Routes from './routes'
+import sampleData from './initialState.json'
+import storeFactory from './store'
+import {Provider} from 'react-redux' // to pass store to the component tree
+
+const initialState = (localStorage["redux-store"]) ?
+  JSON.parse(localStorage["redux-store"]) :
+  sampleData
+
+const saveState = () =>
+  localStorage["redux-store"] = JSON.stringify(store.getState())
+
+const store = storeFactory(initialState)
+store.subscribe(saveState)
+
+// window.React = React
+window.store = store
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes/>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes/>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('react-container')
 )
